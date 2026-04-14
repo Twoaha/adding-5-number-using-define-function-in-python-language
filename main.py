201c443701994bc3ba6262fb8f90804dc232e55a
Twoaha Ibn Jalal <108892202+Twoaha@users.noreply.github.com>
2026-04-15 01:57:03 +0600

    main.py
    adding-5-number-using-define-function-in-python-language
    Hello, I am twoaha this is a python code about how to add 5 number , there are two codes available for learning same topic but code have some changes.

---
+++ main.py
@@ -0,0 +1,27 @@
+#Easy Method :
+# add 5 number using ython
+print("Add 5 number using python method 1")
+def add_number1():
+    num1=float(input("Enter a number :"))
+    num2=float(input("Enter an another number :"))
+    num3=float(input("Enter an another number :"))
+    num4=float(input("Enter an another number :"))
+    num5=float(input("Enter an another number :"))
+    total=num1+num2+num3+num4+num5
+    print('The addition of 5 number is :',total)
+add_number1()        
+
+#Another Method :
+# add 5 number using ython
+print("Add 5 number using python method 2")
+def add_number2():
+    numbers=[]
+    for i in range(5):
+        num=float(input(f'Enter a number {i+1} :'))
+        #num=float(input(f'{i+1} | Enter a number :'))
+        numbers.append(num)
+        total=sum(numbers)
+    print(f'The addition of 5 number is : {total}')
+    #print('The addition of 5 number is :',total)
+    
+add_number2()
